40080d3cd961130153337c4fa6b1e06c170668c8
yamileandrea <ydiaz44@email.arizona.edu>
2015-04-23 08:21:36 -0700

    packrat

--- packrat/lib/x86_64-apple-darwin13.4.0/3.1.2/httr/tests/testthat/test-content.R
+++
@@ -1,6 +0,0 @@
-context("Content")
-
-test_that("parse content is null if no body", {
-  out <- content(HEAD("http://httpbin.org/headers"))
-  expect_equal(out, NULL)
-})
--- packrat/lib/x86_64-apple-darwin13.4.0/3.1.2/packrat/resources/init-rprofile.R
+++
@@ -1,3 +0,0 @@
-#### -- Packrat Autoloader (version 0.4.2-6) -- ####
-source("packrat/init.R")
-#### -- End Packrat Autoloader -- ####
